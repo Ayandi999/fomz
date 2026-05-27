bd4eba0b0600f504f21d8423cf72a281d9ab45ff
Ayan-dip <ayandip6969@gmail.com>
2026-05-27 11:54:40 +0530

    tsup

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/index.ts"],
   noExternal: ["@repo"], // transpile internal monorepo packages
+  external: ["argon2"], // never bundle native C++ modules
   splitting: false,
   bundle: true,
   outDir: "./dist",
